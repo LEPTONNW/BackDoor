2b11cc0ad1dfbf84c76d977080d8ce10f3c99ee7
Round Robin <kac4484@gmail.com>
2024-07-01 17:36:28 +0900

    Add files via upload

---
+++ AKIRA_F_Clt/AKIRA_F_Clt/AKIRA_F_Clt/Remote.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AKIRA_F_Clt
+{
+    public partial class Remote : Form
+    {
+        public Remote()
+        {
+            InitializeComponent();
+        }
+
+        public static string Passvalue2 { get; set; }
+
+        private void Remote_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Passvalue2 = "/help";
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Passvalue2 = "STT$:";
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Passvalue2 = "STD$:";
+            this.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Passvalue2 = "KLO$:";
+            this.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Passvalue2 = "KLN$:";
+            this.Close();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Passvalue2 = "RMT$:";
+            this.Close();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Passvalue2 = "RMN$:";
+            this.Close();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Passvalue2 = "CAP$:";
+            this.Close();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Passvalue2 = "KILL$:";
+            this.Close();
+        }
+    }
+}
